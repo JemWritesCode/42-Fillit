43e6ec3528aa444fdb67714683b4d7d96b6297a3
jcope <thejemmeh@gmail.com>
2019-02-09 19:00:40 -0800

    valid file

---
+++ ProjectFiles/fillitproject/srcs/valid.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   valid.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jcope <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/09 18:57:54 by jcope             #+#    #+#             */
+/*   Updated: 2019/02/09 19:00:25 by jcope            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+// Make sure tetrominos are all valid. 
+	- Check File Formatting (4 lines then a newline)
+	- Only 4 # per piece
+	- Count Method (6 touching sides per piece / 8 in the square)
+*/
+
