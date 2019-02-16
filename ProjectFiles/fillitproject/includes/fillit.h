cb0eadadf943586143fab6302f3f0c7281f07480
tasertasertaser <cschulle@student.42.us.org>
2019-02-15 16:37:49 -0800

    updated piece struct

--- ProjectFiles/fillitproject/includes/fillit.h
+++ ProjectFiles/fillitproject/includes/fillit.h
@@ -6,7 +6,7 @@
 /*   By: jcope <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/06 18:08:47 by jcope             #+#    #+#             */
-/*   Updated: 2019/02/12 17:36:38 by cschulle         ###   ########.fr       */
+/*   Updated: 2019/02/15 16:37:02 by cschulle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,14 @@
 
 char	*parser(char *filename);
 
-typedef struct	s_piece
+typedef struct		s_piece
 {
-	int			width;
-	int			height;
-	char		**piecemap;
-	char		pieceletter;
-}				t_piece;
+	int				width;
+	int				height;
+	char			**piecemap;
+	char			pieceletter;
+	struct s_piece	next;
+}					t_piece;
 
 typedef struct	s_map
 {
