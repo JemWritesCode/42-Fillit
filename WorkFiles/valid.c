99866f423be3c82a73a819b8c8ad504c9c1371bf
tasertasertaser <cschulle@student.42.us.org>
2019-02-20 19:43:29 -0800

    updated header with shifter prototypes

--- WorkFiles/valid.c
+++ WorkFiles/valid.c
@@ -6,7 +6,7 @@
 /*   By: jcope <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/09 18:57:54 by jcope             #+#    #+#             */
-/*   Updated: 2019/02/16 23:13:12 by cschulle         ###   ########.fr       */
+/*   Updated: 2019/02/17 19:35:32 by cschulle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,44 +67,11 @@ int		adjacency_counter(char* buf)
 	return(count);
 }
 
-#include <stdio.h>
-
-int main(void)
+int		valid(char *buf)
 {
-	char *buf1 = "....\n....\n....\n####\n\n"; // valid 
-	char *buf2 = "...\n....\n....\n####\n\n"; // line too short
-	char *buf3 = "...\n....\n....\n####\n....\n\n"; // block too long
-	char *buf4 = "...\n....\n....\n#A##\n\n"; // invalid character
-	char *buf5 = "....\n....\n....\n#.##\n\n"; // invalid tetromino
-	//char *buf6 = ""; // empty buf
-	if (charcount(buf1) == 4)
-		printf("charcount works on valid input :)\n");
-	else
-		printf("charcount broke :(\n");
-
-	if (adjacency_counter(buf1) == 6 || adjacency_counter(buf1) == 8)
-		printf("adjacency counter works on valid input :)\n");
-	else
-		printf("adjacency counter broke :(\n");
-	
-	if (charcount(buf2) != 4)
-		printf("charcount detects line too short :)\n");
-	else
-		printf("charcount broke :(\n");
-
-	if (charcount(buf3) != 4)
-		printf("charcount detects block too long :)\n");
-	else
-		printf("charcount broke :(\n");
-
-	if (charcount(buf4) != 4)
-		printf("charcount detects invalid character :)\n");
-	else
-		printf("charcount broke :(\n");
-
-	if (adjacency_counter(buf5) != 6 && adjacency_counter(buf5) != 8)
-		printf("adjacency counter detects invalid piece :)\n");
-	else
-		printf("adjacency counter broke :(\n");
-	return (0);
+	if(charcount(buf) != 4)
+		return(0);
+	if(adjacency_counter(buf) != 6 && adjacency_counter(buf) != 8)
+		return(0);
+	return(1);
 }
