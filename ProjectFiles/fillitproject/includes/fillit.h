73339fbc580a979258fe9db134b8b5e20fce54c8
tasertasertaser <cschulle@student.42.us.org>
2019-02-27 22:28:01 -0800

    updated valid() prototype

--- ProjectFiles/fillitproject/includes/fillit.h
+++ ProjectFiles/fillitproject/includes/fillit.h
@@ -6,7 +6,7 @@
 /*   By: jcope <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/06 18:08:47 by jcope             #+#    #+#             */
-/*   Updated: 2019/02/21 21:10:08 by cschulle         ###   ########.fr       */
+/*   Updated: 2019/02/27 22:27:46 by cschulle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,6 +42,6 @@ t_map			*solve(t_piece *piecelist);
 t_piece			*parser(char *filename);
 void			shift_x(t_piece *piece, int n);
 void			shift_y(t_piece *piece, int y);
-int				valid(char *buf);
+int				valid(char *buf, int size);
 
 #endif
