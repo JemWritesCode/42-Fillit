091b5af91b27dd84a63834096dfcd533e2df0f5d
jcope <thejemmeh@gmail.com>
2019-03-06 21:02:56 -0800

    Normed map file except the free_map function I may still need to write.

--- ProjectFiles/fillitproject/srcs/map.c
+++ ProjectFiles/fillitproject/srcs/map.c
@@ -29,23 +29,26 @@ int		round_up_sq_rt(int num)
 	return (map_size);
 }
 
+/*
+** Use malloc to make space for the map.
+** Use malloc to make space for 2D map array.
+** then you need to malloc out the rows of the map based on map_size as well,
+** plus newline with strnew(). Set it all to .
+*/
+
 t_map	*new_map(int map_size)
 {
-	t_map *map;
-	int i;
+	t_map	*map;
+	int		i;
 
-	map = (t_map *)ft_memalloc(sizeof(t_map)); //malloc a map the map_size of map struct.
-
-	// map_size of map * char pointer. 
-	//Turn this into ** so it's basically like the first column of the map.
-	map->array = (char**)ft_memalloc(sizeof(char*) * map_size); 
-	//then you need to malloc out the rows of the map based on map_size as well, plus newline.
+	map = (t_map *)ft_memalloc(sizeof(t_map));
+	map->array = (char**)ft_memalloc(sizeof(char*) * map_size);
 	i = 0;
 	while (i < map_size)
 	{
-		map->array[i] = ft_strnew(map_size); 
-		ft_memset(map->array[i], '.', map_size); // fill in each row with .
-		i++; // go to the next row. 
+		map->array[i] = ft_strnew(map_size);
+		ft_memset(map->array[i], '.', map_size);
+		i++;
 	}
 	return (map);
 }
@@ -55,19 +58,20 @@ size_t	count_pieces(t_piece *piecelist)
 	size_t	count;
 
 	count = 0;
-	while (piecelist != NULL)
+	while (piecelist)
 	{
 		piecelist = piecelist->next;
 		count++;
 	}
-	return(count);
+	return (count);
 }
 
 /*
-* Print the map one row at a time followed by newline.
+** Print the map one row at a time followed by newline.
 */
+
 void	print_map(t_map *map, int size)
-{	
+{
 	int i;
 
 	i = 0;
